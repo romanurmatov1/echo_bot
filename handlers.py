f9f331ab3dea924b43ec6526a75ddc315831c106
ufo607 <ufo607.off@gmail.com>
2021-10-29 19:38:42 +0500

    first commit

---
+++ handlers.py
@@ -0,0 +1,22 @@
+from app import bot, dp
+from config import admin_id
+
+from aiogram.types import Message
+from config import admin_id
+
+async def send_to_admin(dp):
+   await bot.send_message(chat_id=admin_id, text="Salom Raxmatjon bot ishga tushdi")
+word = ['Roma', 'Raxmatjon', 'roma', 'raxmatjon', 'Rahmatjon', 'rahmatjon', 'Juventus', 'Yuventus', 'Juve', 'Yuve', 'ufo607', 'ufo 607', 'ufo_607', 'Ufo607', 'Ufo_607', 'Ufo 607', 'Roma Borz', 'roma Borz', 'roma borz']
+neg = ['Barcelona', 'Barselona', 'barcelona', 'barselona', 'barca', 'barsa', 'Barca', 'Barsa', 'Real', 'Real Madrid', 'real', 'real madrid','realmadrid', 'RealMadrid']
+@dp.message_handler()
+async def echo(message: Message):
+   count = 0
+   text = message.text
+   if text in word:
+      await bot.send_message(chat_id=admin_id, text=f"'{text}' best of the best")
+   elif text in neg:
+      await bot.send_message(chat_id=admin_id, text=f"'{text}' bad of the bad")
+   else:
+      await bot.send_message(chat_id=admin_id, text=text)
+   
+   # await message.answer(text=text)
